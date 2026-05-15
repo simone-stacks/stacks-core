simone-stacks/stacks-core

commit 4e306ce17c847e7518caf90b133b7b0bbd23395e

feat: new libsigner implementation.  Stacker signers and sBTC signers would use this crate to implement a Stacks event receiver and a runloop.

libsigner/src/runloop.rs
@@ -1,0 +1,244 @@
+// Copyright (C) 2013-2020 Blockstack PBC, a public benefit corporation
+// Copyright (C) 2020-2023 Stacks Open Internet Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#![allow(unused_imports)]
+#![allow(dead_code)]
+
+use std::marker::PhantomData;
+use std::net::SocketAddr;
+use std::sync::mpsc::{channel, Receiver, RecvTimeoutError};
+use std::thread;
+use std::thread::JoinHandle;
+use std::time::Duration;
+
+use crate::events::{EventReceiver, EventStopSignaler, StackerDBChunksEvent};
+
+use crate::error::EventError;
+
+use stacks_common::deps_common::ctrlc as termination;
+use stacks_common::deps_common::ctrlc::SignalId;
+
+use libc;
+
+/// Some libcs, like musl, have a very small stack size.
+/// Make sure it's big enough.
+const THREAD_STACK_SIZE: usize = 128 * 1024 * 1024; // 128 MB
+
+/// stderr fileno
+const STDERR: i32 = 2;
+
+/// Trait describing the needful components of a top-level runloop.
+/// This is where the signer business logic would go.
+/// Implement this, and you get all the multithreaded setup for free.
+pub trait SignerRunLoop<R> {
+    /// Hint to set how long to wait for new events
+    fn set_event_timeout(&mut self, timeout: Duration);
+    /// Getter for the event poll timeout
+    fn get_event_timeout(&self) -> Duration;
+    /// Run one pass of the event loop, given new StackerDB events discovered since the last pass.
+    /// Returns Some(R) if this is the final pass -- the runloop evaluated to R
+    /// Returns None to keep running.
+    fn run_one_pass(&mut self, events: Option<StackerDBChunksEvent>) -> Option<R>;
+
+    /// This is the main loop body for the signer. It continuously receives events from
+    /// `event_recv`, polling for up to `self.get_event_timeout()` units of time.  Once it has
+    /// polled for events, they are fed into `run_one_pass()`.  This continues until either
+    /// `run_one_pass()` returns `false`, or the event receiver hangs up.  At this point, this
+    /// method calls the `event_stop_signaler.send()` to terminate the receiver.
+    ///
+    /// This would run in a separate thread from the event receiver.
+    fn main_loop<EVST: EventStopSignaler>(
+        &mut self,
+        event_recv: Receiver<StackerDBChunksEvent>,
+        mut event_stop_signaler: EVST,
+    ) -> Option<R> {
+        loop {
+            let poll_timeout = self.get_event_timeout();
+            let next_event_opt = match event_recv.recv_timeout(poll_timeout) {
+                Ok(event) => Some(event),
+                Err(RecvTimeoutError::Timeout) => None,
+                Err(RecvTimeoutError::Disconnected) => {
+                    info!("Event receiver disconnected");
+                    return None;
+                }
+            };
+            if let Some(final_state) = self.run_one_pass(next_event_opt) {
+                // finished!
+                info!("Runloop exit; signaling event-receiver to stop");
+                event_stop_signaler.send();
+                return Some(final_state);
+            }
+        }
+    }
+}
+
+/// The top-level signer implementation
+pub struct Signer<R, SL: SignerRunLoop<R> + Send + Sync, EV: EventReceiver + Send> {
+    /// the runloop itself
+    signer_loop: Option<SL>,
+    /// the event receiver to use
+    event_receiver: Option<EV>,
+    /// marker to permit the R type
+    _phantom: PhantomData<R>,
+}
+
+/// The running signer implementation
+pub struct RunningSigner<EV: EventReceiver, R> {
+    /// join handle for signer runloop
+    signer_join: JoinHandle<Option<R>>,
+    /// join handle for event receiver
+    event_join: JoinHandle<()>,
+    /// kill signal for the event receiver
+    stop_signal: EV::ST,
+}
+
+impl<EV: EventReceiver, R> RunningSigner<EV, R> {
+    /// Stop the signer, and get the final state
+    pub fn stop(mut self) -> Option<R> {
+        // kill event receiver
+        self.stop_signal.send();
+
+        debug!("Try join event loop...");
+        // wait for event receiver join
+        let _ = self.event_join.join().map_err(|thread_panic| {
+            error!("Event thread panicked with: '{:?}'", &thread_panic);
+            thread_panic
+        });
+        info!("Event receiver thread joined");
+
+        // wait for runloop to join
+        debug!("Try join signer loop...");
+        let result_opt = self
+            .signer_join
+            .join()
+            .map_err(|thread_panic| {
+                error!("Event thread panicked with: '{:?}'", &thread_panic);
+                thread_panic
+            })
+            .unwrap_or(None);
+
+        info!("Signer thread joined");
+        result_opt
+    }
+}
+
+/// Write to stderr in an async-safe manner.
+/// See signal-safety(7)
+#[warn(unused)]
+fn async_safe_write_stderr(msg: &str) {
+    #[cfg(windows)]
+    unsafe {
+        // write(2) inexplicably has a different ABI only on Windows.
+        libc::write(
+            STDERR,
+            msg.as_ptr() as *const libc::c_void,
+            msg.len() as u32,
+        );
+    }
+    #[cfg(not(windows))]
+    unsafe {
+        libc::write(STDERR, msg.as_ptr() as *const libc::c_void, msg.len());
+    }
+}
+
+/// This is a termination handler for handling receipt of a terminating UNIX signal, like SIGINT,
+/// SIGQUIT, SIGTERM, or SIGBUS.  You'd call this as part of the startup code for the signer daemon.
+/// DO NOT call this from within the library!
+pub fn set_runloop_signal_handler<ST: EventStopSignaler + Send + 'static>(mut stop_signaler: ST) {
+    termination::set_handler(move |sig_id| match sig_id {
+        SignalId::Bus => {
+            let msg = "Caught SIGBUS; crashing immediately and dumping core\n";
+            async_safe_write_stderr(msg);
+            unsafe {
+                libc::abort();
+            }
+        }
+        _ => {
+            let msg = format!("Graceful termination request received (signal `{}`), will complete the ongoing runloop cycles and terminate\n", sig_id);
+            async_safe_write_stderr(&msg);
+            stop_signaler.send();
+        }
+    }).expect("FATAL: failed to set signal handler");
+}
+
+impl<
+        R: Send + 'static,
+        SL: SignerRunLoop<R> + Send + Sync + 'static,
+        EV: EventReceiver + Send + 'static,
+    > Signer<R, SL, EV>
+{
+    pub fn new(runloop: SL, event_receiver: EV) -> Signer<R, SL, EV> {
+        Signer {
+            signer_loop: Some(runloop),
+            event_receiver: Some(event_receiver),
+            _phantom: PhantomData,
+        }
+    }
+
+    /// This is a helper function to spawn both the runloop and event receiver in their own
+    /// threads.  Advanced signers may not need this method, and instead opt to run the receiver
+    /// and runloop directly.  However, this method is present to help signer developers to get
+    /// their implementations off the ground.
+    ///
+    /// The given `bind_addr` is the server address this event receiver needs to listen on, so the
+    /// stacks node can POST events to it.
+    ///
+    /// On success, this method consumes the Signer and returns a RunningSigner with the relevant
+    /// inter-thread communication primitives for the caller to shut down the system.
+    pub fn spawn(&mut self, bind_addr: SocketAddr) -> Result<RunningSigner<EV, R>, EventError> {
+        let mut event_receiver = self
+            .event_receiver
+            .take()
+            .ok_or(EventError::AlreadyRunning)?;
+        let mut signer_loop = self.signer_loop.take().ok_or(EventError::AlreadyRunning)?;
+
+        let (event_send, event_recv) = channel();
+        event_receiver.add_consumer(event_send);
+
+        event_receiver.bind(bind_addr)?;
+        let stop_signaler = event_receiver.get_stop_signaler()?;
+        let mut ret_stop_signaler = event_receiver.get_stop_signaler()?;
+
+        // start a thread for the event receiver
+        let event_thread = thread::Builder::new()
+            .name("event_receiver".to_string())
+            .stack_size(THREAD_STACK_SIZE)
+            .spawn(move || event_receiver.main_loop())
+            .map_err(|e| {
+                error!("EventReceiver failed to start: {:?}", &e);
+                EventError::FailedToStart
+            })?;
+
+        // start receiving events and doing stuff with them
+        let runloop_thread = thread::Builder::new()
+            .name("signer_runloop".to_string())
+            .stack_size(THREAD_STACK_SIZE)
+            .spawn(move || signer_loop.main_loop(event_recv, stop_signaler))
+            .map_err(|e| {
+                error!("SignerRunLoop failed to start: {:?}", &e);
+                ret_stop_signaler.send();
+                EventError::FailedToStart
+            })?;
+
+        let running_signer = RunningSigner {
+            signer_join: runloop_thread,
+            event_join: event_thread,
+            stop_signal: ret_stop_signaler,
+        };
+
+        Ok(running_signer)
+    }
+}

libsigner/Cargo.toml
@@ -1,0 +1,46 @@
+[package]
+name = "libsigner"
+version = "0.0.1"
+authors = [ "Jude Nelson <jude@stacks.org>" ]
+license = "GPLv3"
+homepage = "https://github.com/blockstack/stacks-blockchain"
+repository = "https://github.com/blockstack/stacks-blockchain"
+description = "Library for Stacks signer daemons"
+keywords = [ "stacks", "stx", "bitcoin", "crypto", "blockstack", "decentralized", "dapps", "blockchain" ]
+readme = "README.md"
+resolver = "2"
+edition = "2021"
+
+[lib]
+name = "libsigner"
+path = "./src/libsigner.rs"
+
+[[bin]]
+name = "stackschat"
+path = "demos/stackschat/main.rs"
+
+[dependencies]
+serde = "1"
+serde_derive = "1"
+serde_stacker = "0.1"
+stacks-common = { path = "../stacks-common" }
+clarity = { path = "../clarity" }
+libstackerdb = { path = "../libstackerdb" }
+slog = { version = "2.5.2", features = [ "max_level_trace" ] }
+slog-term = "2.6.0"
+slog-json = { version = "2.3.0", optional = true }
+libc = "0.2"
+
+[dependencies.serde_json]
+version = "1.0"
+features = ["arbitrary_precision", "unbounded_depth"]
+
+[dependencies.secp256k1]
+version = "0.24.3"
+features = ["serde", "recovery"]
+
+[target.'cfg(all(any(target_arch = "x86_64", target_arch = "x86", target_arch = "aarch64"), not(target_env = "msvc")))'.dependencies]
+sha2 = { version = "0.10", features = ["asm"] }
+
+[target.'cfg(any(not(any(target_arch = "x86_64", target_arch = "x86", target_arch = "aarch64")), target_env = "msvc"))'.dependencies]
+sha2 = { version = "0.10" }

libsigner/src/error.rs
@@ -1,0 +1,131 @@
+// Copyright (C) 2013-2020 Blockstack PBC, a public benefit corporation
+// Copyright (C) 2020-2023 Stacks Open Internet Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use std::error;
+use std::fmt;
+use std::io;
+
+/// Errors originating from doing an RPC request to the Stacks node
+#[derive(Debug)]
+pub enum RPCError {
+    IO(io::Error),
+    Deserialize(String),
+    NotConnected,
+    MalformedRequest(String),
+    MalformedResponse(String),
+    HttpError(u32),
+}
+
+impl fmt::Display for RPCError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            RPCError::IO(ref s) => fmt::Display::fmt(s, f),
+            RPCError::Deserialize(ref s) => fmt::Display::fmt(s, f),
+            RPCError::HttpError(ref s) => {
+                write!(f, "HTTP code {}", s)
+            }
+            RPCError::MalformedRequest(ref s) => {
+                write!(f, "Malformed request: {}", s)
+            }
+            RPCError::MalformedResponse(ref s) => {
+                write!(f, "Malformed response: {}", s)
+            }
+            RPCError::NotConnected => {
+                write!(f, "Not connected")
+            }
+        }
+    }
+}
+
+impl error::Error for RPCError {
+    fn cause(&self) -> Option<&dyn error::Error> {
+        match *self {
+            RPCError::IO(ref s) => Some(s),
+            RPCError::Deserialize(..) => None,
+            RPCError::HttpError(..) => None,
+            RPCError::MalformedRequest(..) => None,
+            RPCError::MalformedResponse(..) => None,
+            RPCError::NotConnected => None,
+        }
+    }
+}
+
+impl From<io::Error> for RPCError {
+    fn from(e: io::Error) -> RPCError {
+        RPCError::IO(e)
+    }
+}
+
+/// Errors originating from receiving event data from the Stacks node
+#[derive(Debug)]
+pub enum EventError {
+    IO(io::Error),
+    Deserialize(String),
+    MalformedRequest(String),
+    NotBound,
+    Terminated,
+    AlreadyRunning,
+    FailedToStart,
+    UnrecognizedEvent(String),
+}
+
+impl fmt::Display for EventError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            EventError::IO(ref s) => fmt::Display::fmt(s, f),
+            EventError::Deserialize(ref s) => fmt::Display::fmt(s, f),
+            EventError::MalformedRequest(ref s) => {
+                write!(f, "Malformed request: {}", s)
+            }
+            EventError::NotBound => {
+                write!(f, "Not bound to a port yet")
+            }
+            EventError::Terminated => {
+                write!(f, "Listener is terminated")
+            }
+            EventError::AlreadyRunning => {
+                write!(f, "Thread already running")
+            }
+            EventError::FailedToStart => {
+                write!(f, "Failed to start thread")
+            }
+            EventError::UnrecognizedEvent(ref ev) => {
+                write!(f, "Unrecognized event '{}'", &ev)
+            }
+        }
+    }
+}
+
+impl error::Error for EventError {
+    fn cause(&self) -> Option<&dyn error::Error> {
+        match *self {
+            EventError::IO(ref s) => Some(s),
+            EventError::Deserialize(..) => None,
+            EventError::MalformedRequest(..) => None,
+            EventError::NotBound => None,
+            EventError::Terminated => None,
+            EventError::AlreadyRunning => None,
+            EventError::FailedToStart => None,
+            EventError::UnrecognizedEvent(..) => None,
+        }
+    }
+}
+
+impl From<io::Error> for EventError {
+    fn from(e: io::Error) -> EventError {
+        EventError::IO(e)
+    }
+}

libsigner/src/events.rs
@@ -1,0 +1,268 @@
+// Copyright (C) 2013-2020 Blockstack PBC, a public benefit corporation
+// Copyright (C) 2020-2023 Stacks Open Internet Foundation
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use std::sync::mpsc::Sender;
+
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
+
+use std::net::SocketAddr;
+use std::net::TcpListener;
+use std::net::TcpStream;
+
+use std::io::Read;
+
+use clarity::vm::types::QualifiedContractIdentifier;
+use libstackerdb::StackerDBChunkData;
+
+use serde::{Deserialize, Serialize};
+
+use crate::http::{decode_http_body, decode_http_request};
+use crate::EventError;
+
+/// Event structure for newly-arrived StackerDB data
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct StackerDBChunksEvent {
+    pub contract_id: QualifiedContractIdentifier,
+    pub modified_slots: Vec<StackerDBChunkData>,
+}
+
+/// Trait to implement a stop-signaler for the event receiver thread.
+/// The caller calls `send()` and the event receiver loop (which lives in a separate thread) will
+/// terminate.
+pub trait EventStopSignaler {
+    fn send(&mut self);
+}
+
+/// Trait to implement to handle StackerDB events sent by the Stacks node
+pub trait EventReceiver {
+    /// The implementation of ST will ensure that a call to ST::send() will cause
+    /// the call to `is_stopped()` below to return true.
+    type ST: EventStopSignaler + Send + Sync;
+
+    /// Open a server socket to the given socket address.
+    fn bind(&mut self, listener: SocketAddr) -> Result<SocketAddr, EventError>;
+    /// Return the next event
+    fn next_event(&mut self) -> Result<StackerDBChunksEvent, EventError>;
+    /// Add a downstream event consumer
+    fn add_consumer(&mut self, event_out: Sender<StackerDBChunksEvent>);
+    /// Forward the event to downstream consumers
+    fn forward_event(&mut self, ev: StackerDBChunksEvent) -> bool;
+    /// Determine if the receiver should hang up
+    fn is_stopped(&self) -> bool;
+    /// Get a stop signal instance that, when sent, will cause this receiver to stop accepting new
+    /// events.  Called after `bind()`.
+    fn get_stop_signaler(&mut self) -> Result<Self::ST, EventError>;
+
+    /// Main loop for the receiver.
+    /// Typically, this is started in a separate thread.
+    fn main_loop(&mut self) {
+        loop {
+            if self.is_stopped() {
+                info!("Event receiver stopped");
+                break;
+            }
+            let next_event = match self.next_event() {
+                Ok(event) => event,
+                Err(EventError::UnrecognizedEvent(..)) => {
+                    // got an event that we don't care about (not a problem)
+                    continue;
+                }
+                Err(EventError::Terminated) => {
+                    // we're done
+                    info!("Caught termination signal");
+                    break;
+                }
+                Err(e) => {
+                    warn!("Failed to receive next event: {:?}", &e);
+                    continue;
+                }
+            };
+            if !self.forward_event(next_event) {
+                info!("Failed to forward event");
+                break;
+            }
+        }
+        info!("Event receiver main loop exit");
+    }
+}
+
+pub struct StackerDBEventReceiver {
+    /// contracts we're listening for
+    pub stackerdb_contract_ids: Vec<QualifiedContractIdentifier>,
+    /// Address we bind to
+    local_addr: Option<SocketAddr>,
+    /// server socket that listens for HTTP POSTs from the node
+    sock: Option<TcpListener>,
+    /// channel into which to write newly-discovered data
+    out_channels: Vec<Sender<StackerDBChunksEvent>>,
+    /// inter-thread stop variable -- if set to true, then the `main_loop` will exit
+    stop_signal: Arc<AtomicBool>,
+}
+
+impl StackerDBEventReceiver {
+    /// Make a new StackerDB event receiver, and return both the receiver and the read end of a
+    /// channel into which node-received data can be obtained.
+    pub fn new(contract_ids: Vec<QualifiedContractIdentifier>) -> StackerDBEventReceiver {
+        let stackerdb_receiver = StackerDBEventReceiver {
+            stackerdb_contract_ids: contract_ids,
+            sock: None,
+            local_addr: None,
+            out_channels: vec![],
+            stop_signal: Arc::new(AtomicBool::new(false)),
+        };
+        stackerdb_receiver
+    }
+
+    /// Do something with the socket
+    pub fn with_socket<F, R>(&mut self, todo: F) -> Result<R, EventError>
+    where
+        F: FnOnce(&mut StackerDBEventReceiver, &mut TcpListener) -> R,
+    {
+        let mut sock = if let Some(s) = self.sock.take() {
+            s
+        } else {
+            return Err(EventError::NotBound);
+        };
+
+        let res = todo(self, &mut sock);
+
+        self.sock = Some(sock);
+        Ok(res)
+    }
+}
+
+/// Stop signaler implementation
+pub struct StackerDBStopSignaler {
+    stop_signal: Arc<AtomicBool>,
+    local_addr: SocketAddr,
+}
+
+impl StackerDBStopSignaler {
+    pub fn new(sig: Arc<AtomicBool>, local_addr: SocketAddr) -> StackerDBStopSignaler {
+        StackerDBStopSignaler {
+            stop_signal: sig,
+            local_addr,
+        }
+    }
+}
+
+impl EventStopSignaler for StackerDBStopSignaler {
+    fn send(&mut self) {
+        self.stop_signal.store(true, Ordering::SeqCst);
+
+        // wake up the thread so the atomicbool can be checked
+        let _ = TcpStream::connect(&self.local_addr);
+    }
+}
+
+impl EventReceiver for StackerDBEventReceiver {
+    type ST = StackerDBStopSignaler;
+
+    /// Start listening on the given socket address.
+    /// Returns the address that was bound.
+    /// Errors out if bind(2) fails
+    fn bind(&mut self, listener: SocketAddr) -> Result<SocketAddr, EventError> {
+        let srv = TcpListener::bind(listener)?;
+        let bound_addr = srv.local_addr()?;
+        self.sock = Some(srv);
+        self.local_addr = Some(bound_addr.clone());
+        Ok(bound_addr)
+    }
+
+    /// Wait for the node to post something, and then return it.
+    /// Errors are recoverable -- the caller should call this method again even if it returns an
+    /// error.
+    fn next_event(&mut self) -> Result<StackerDBChunksEvent, EventError> {
+        self.with_socket(|event_receiver, server_sock| {
+            let (mut node_sock, _) = server_sock.accept()?;
+
+            // were we asked to terminate?
+            if event_receiver.is_stopped() {
+                return Err(EventError::Terminated);
+            }
+
+            let mut buf = vec![];
+            node_sock.read_to_end(&mut buf)?;
+
+            let (verb, path, headers, body_offset) = decode_http_request(&buf)?;
+            if verb != "POST" {
+                return Err(EventError::MalformedRequest(format!(
+                    "Unrecognized verb '{}'",
+                    &verb
+                )));
+            }
+            if path != "/stackerdb_chunks" {
+                return Err(EventError::UnrecognizedEvent(path));
+            }
+
+            let body = decode_http_body(&headers, &buf[body_offset..])?;
+            let event: StackerDBChunksEvent = serde_json::from_slice(&body).map_err(|e| {
+                EventError::Deserialize(format!("Could not decode body to JSON: {:?}", &e))
+            })?;
+            Ok(event)
+        })?
+    }
+
+    /// Determine if the receiver is hung up
+    fn is_stopped(&self) -> bool {
+        self.stop_signal.load(Ordering::SeqCst)
+    }
+
+    /// Forward an event
+    /// Return true on success; false on error.
+    /// Returning false terminates the event receiver.
+    fn forward_event(&mut self, ev: StackerDBChunksEvent) -> bool {
+        if self.out_channels.len() == 0 {
+            // nothing to do
+            error!("No channels connected to event receiver");
+            return false;
+        } else if self.out_channels.len() == 1 {
+            // avoid a clone
+            if let Err(e) = self.out_channels[0].send(ev) {
+                error!("Failed to send to signer runloop: {:?}", &e);
+                return false;
+            }
+            return true;
+        } else {
+            for (i, out_channel) in self.out_channels.iter().enumerate() {
+                if let Err(e) = out_channel.send(ev.clone()) {
+                    error!("Failed to send to signer runloop #{}: {:?}", i, &e);
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    /// Add an event consumer.  A received event will be forwarded to this Sender.
+    fn add_consumer(&mut self, out_channel: Sender<StackerDBChunksEvent>) {
+        self.out_channels.push(out_channel);
+    }
+
+    /// Get a stopped signaler.  The caller can then use it to terminate the event receiver loop,
+    /// even if it's in a different thread.
+    fn get_stop_signaler(&mut self) -> Result<StackerDBStopSignaler, EventError> {
+        if let Some(local_addr) = self.local_addr.as_ref() {
+            Ok(StackerDBStopSignaler::new(
+                self.stop_signal.clone(),
+                local_addr.clone(),
+            ))
+        } else {
+            Err(EventError::NotBound)
+        }
+    }
+}

commit db4a2ffbdeaabfac6a3c02196dda45e5a0dd913b

doc: correct main_loop comment about run_one_pass return value
SignerRunLoop trait's run_one_pass method returns Option<R>, but
main_loop's doc-comment claims the loop continues until
`run_one_pass()` returns 'false'. The loop body actually checks
'if let Some(final_state) = self.run_one_pass(...)' and exits on the
Some(R) variant. The comment has been wrong since the libsigner crate
was introduced in commit 4e306ce (2023-08-28); the function was
declared as returning Option<R> from the start, so the 'false' in
the doc was always inconsistent with the type signature.

libsigner/src/runloop.rs
@@ -52,7 +52,7 @@ pub trait SignerRunLoop<R: Send, T: SignerEventTrait> {
     /// This is the main loop body for the signer. It continuously receives events from
     /// `event_recv`, polling for up to `self.get_event_timeout()` units of time.  Once it has
     /// polled for events, they are fed into `run_one_pass()`.  This continues until either
-    /// `run_one_pass()` returns `false`, or the event receiver hangs up.  At this point, this
+    /// `run_one_pass()` returns `Some(R)`, or the event receiver hangs up.  At this point, this
     /// method calls the `event_stop_signaler.send()` to terminate the receiver.
     ///
     /// This would run in a separate thread from the event receiver.